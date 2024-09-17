678f8b52d7705bf585af02fdb9e3cead9e1bcb0a
qrxnz <send@qrxnz.dev>
2024-09-17 22:38:21 +0200

    Update dap.lua

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -19,25 +19,43 @@ return {
 
 		dap.adapters.lldb = {
 			type = "executable",
-			command = "/usr/bin/lldb-vscode", -- adjust as needed, must be absolute path
+			command = "/usr/bin/lldb", -- adjust as needed, must be absolute path
 			name = "lldb",
 		}
 
-		dap.configurations.c = {
+		dap.adapters.codelldb = {
+			type = "server",
+			host = "127.0.0.1",
+			port = 13000, -- 💀 Use the port printed out or specified with `--port`
+		}
+
+		dap.adapters.codelldb = {
+			type = "server",
+			port = "${port}",
+			executable = {
+				-- CHANGE THIS to your path!
+				command = "/usr/bin/codelldb",
+				args = { "--port", "${port}" },
+
+				-- On windows you may have to uncomment this:
+				-- detached = false,
+			},
+		}
+
+		dap.configurations.cpp = {
 			{
-				name = "Launch",
-				type = "lldb",
+				name = "Launch file",
+				type = "codelldb",
 				request = "launch",
 				program = function()
 					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 				end,
 				cwd = "${workspaceFolder}",
 				stopOnEntry = false,
-				args = {},
 			},
 		}
-
-		dap.configurations.cpp = dap.configurations.c
+		dap.configurations.c = dap.configurations.cpp
+		dap.configurations.rust = dap.configurations.cpp
 
 		dap.listeners.before.attach.dapui_config = function()
 			dapui.open()
