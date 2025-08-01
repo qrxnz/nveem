4657903212fdf21959cce21b1dbc95bb37c96400
Karol Skoczyk <send@qrxnz.dev>
2025-08-01 12:59:19 +0200

    feat: Configure nvterm for default horizontal terminal layout
    Configured the nvterm plugin to use a horizontal split as the default terminal layout.
    Adjusted the split ratio for better visibility and explicitly set the default type to horizontal.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,10 +1,17 @@
 return {
 	"NvChad/nvterm",
 	keys = {
-		{ "<leader>ca", '<cmd>lua require("nvterm.terminal").toggle "float"<cr>', desc = "Toggle Terminal" },
+		{ "<leader>ca", '<cmd>lua require("nvterm.terminal").toggle "horizontal"<cr>', desc = "Toggle Terminal" },
 	},
 	config = function()
-		require("nvterm").setup({})
+		require("nvterm").setup({
+			terminals = {
+				default_type = "horizontal",
+				horizontal = {
+					split_ratio = 0.4,
+				},
+			},
+		})
 
 		vim.cmd([[ tnoremap <Esc> <C-\><C-n> ]])
 	end,
