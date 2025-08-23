cb15eb911ef34980940bd1f3edf94ac5a7481988
Karol Skoczyk <send@qrxnz.dev>
2025-08-23 16:40:14 +0200

    feat: Simplify DAP debugger configuration
    Removed the codelldb adapter and its associated configuration to streamline and simplify the debugging process. The C++ configuration has been updated to use the lldb adapter directly.

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -19,33 +19,14 @@ return {
 
 		dap.adapters.lldb = {
 			type = "executable",
-			command = "/usr/bin/lldb", -- adjust as needed, must be absolute path
+			command = "lldb",
 			name = "lldb",
-		}
-
-		dap.adapters.codelldb = {
-			type = "server",
-			host = "127.0.0.1",
-			port = 13000, -- 💀 Use the port printed out or specified with `--port`
-		}
-
-		dap.adapters.codelldb = {
-			type = "server",
-			port = "${port}",
-			executable = {
-				-- CHANGE THIS to your path!
-				command = "/usr/bin/codelldb",
-				args = { "--port", "${port}" },
-
-				-- On windows you may have to uncomment this:
-				-- detached = false,
-			},
 		}
 
 		dap.configurations.cpp = {
 			{
 				name = "Launch file",
-				type = "codelldb",
+				type = "lldb",
 				request = "launch",
 				program = function()
 					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
