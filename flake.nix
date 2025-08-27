901e87c031d66d7c9d2f010d958108ef7bfcd4a7
Karol Skoczyk <send@qrxnz.dev>
2025-08-27 18:47:06 +0200

    refactor: Migrate to nixvim
    This commit completely refactors the Neovim configuration from a Lua-based setup using lazy.nvim to a declarative Nix-based configuration using nixvim.
    
    The key changes include:
    
    - Rewriting `flake.nix` to use `nixvim` and `flake-parts`.
    - Deleting the old Lua configuration files (`init.lua`, `lua/`).
    - Creating a new `config/` directory with a modularized `nixvim` configuration.
    - Migrating all plugin configurations to their respective `.nix` files.
    - Updating the `flake.lock` file with the new dependencies.
    
    This change makes the Neovim configuration more reproducible and manageable.

--- flake.nix
+++ flake.nix
@@ -1,63 +1,100 @@
 {
-  description = "qrxnz's Neovim Configuration";
+  description = "A nixvim configuration";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
-    utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:/nixos/nixpkgs/nixos-unstable";
+    nixvim.url = "github:nix-community/nixvim/nixos-24.05";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    utils,
-    ...
-  }:
-    utils.lib.eachDefaultSystem (
-      system: let
-        lib = nixpkgs.lib;
-        pkgs = nixpkgs.legacyPackages.${system};
+  outputs =
+    {
+      nixpkgs,
+      nixpkgs-unstable,
+      nixvim,
+      flake-parts,
+      ...
+    }@inputs:
+    let
+      config = import ./config;
+    in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-        nvim =
-          pkgs.wrapNeovimUnstable (pkgs.neovim-unwrapped)
-          (pkgs.neovimUtils.makeNeovimConfig
-            {
-              customRC = ''
-                set runtimepath^=${./.}
-                source ${./.}/init.lua
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import nixpkgs { inherit system; };
+
+          pkgs-unstable = import nixpkgs-unstable { inherit system; };
+
+          asm-lsp-darwin_overlay = final: prev: {
+            asm-lsp = pkgs-unstable.asm-lsp.overrideAttrs (oldAttrs: {
+              buildInputs =
+                oldAttrs.buildInputs
+                ++ final.lib.optionals final.stdenv.isDarwin [
+                  final.darwin.apple_sdk.frameworks.CoreFoundation
+                  final.darwin.apple_sdk.frameworks.CoreServices
+                  final.darwin.apple_sdk.frameworks.SystemConfiguration
+                ];
+
+              # tests expect ~/.cache/asm-lsp to be writable
+              preCheck = ''
+                export HOME=$(mktemp -d)
               '';
-            }
-            // {
-              wrapperArgs = [
-                "--prefix"
-                "PATH"
-                ":"
-                "${lib.makeBinPath [pkgs.gcc]}"
-              ];
+
+              meta = oldAttrs.meta // {
+                platforms = final.lib.platforms.linux ++ final.lib.platforms.darwin;
+              };
             });
-      in {
-        overlays = {
-          neovim = _: prev: {
-            neovim = nvim;
           };
-          default = self.overlays.neovim;
-        };
 
-        packages = rec {
-          neovim = nvim;
-          default = neovim;
-        };
+          bashdb-darwin_overlay = final: prev: {
+            bashdb = prev.bashdb.overrideAttrs (oldAttrs: {
+              meta = oldAttrs.meta // {
+                platforms = final.lib.platforms.linux ++ final.lib.platforms.darwin;
+              };
+            });
+          };
 
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = [
-            nvim
+          pkgsWithOverlays = pkgs.extend (
+            final: prev: asm-lsp-darwin_overlay final prev // bashdb-darwin_overlay final prev
+          );
 
-            # Formatters
-            pkgs.stylua
-            pkgs.mdformat
-            pkgs.alejandra
-            pkgs.treefmt2
-          ];
+          nixvimLib = nixvim.lib.${system};
+          nvim = nixvim.legacyPackages.${system}.makeNixvimWithModule {
+            pkgs = pkgsWithOverlays;
+            module = config;
+          };
+        in
+        {
+          checks = {
+            default = nixvimLib.check.mkTestDerivationFromNvim {
+              inherit nvim;
+              name = "A nixvim configuration";
+            };
+          };
+
+          packages = {
+            default = nvim;
+          };
+
+          devShells.default = pkgs.mkShellNoCC {
+            shellHook =
+              # bash
+              ''
+                echo Welcome to a Neovim dev environment powered by Nixvim -- https://github.com/nix-community/nixvim
+                PS1="Nixvim: \\w \$ "
+                alias vim='nvim'
+              '';
+            packages = [ nvim ];
+          };
         };
-      }
-    );
+    };
 }
